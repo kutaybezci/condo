b303145992c2a41add3a8bdaed99ca88b4b7013a
kutaybezci <kutay.bezci@gmail.com>
2018-01-23 22:03:40 +0300

    Shell availability implemented

---
+++ condo/src/main/java/com/kutaybezci/condo/AppConfig.java
@@ -0,0 +1,36 @@
+package com.kutaybezci.condo;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class AppConfig {
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.setResultsMapCaseInsensitive(true);
+		return jdbcTemplate;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("locale/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+}
--- condo/src/main/java/com/kutaybezci/condo/shell/UserCommand.java
+++ condo/src/main/java/com/kutaybezci/condo/shell/UserCommand.java
@@ -1,8 +1,10 @@
 package com.kutaybezci.condo.shell;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
 
 import com.kutaybezci.condo.bl.UserBo;
 import com.kutaybezci.condo.bl.exceptions.LoginFailed;
@@ -13,6 +15,7 @@ public class UserCommand {
 
 	@Autowired
 	private UserBo userBo;
+
 	private User user;
 
 	@ShellMethod("For login supply username password")
@@ -20,4 +23,23 @@ public class UserCommand {
 		this.user = userBo.login(userName, password);
 		return String.format("Logged on as: %s", user.getUserName());
 	}
+
+	@ShellMethod(value = "Shows logged on user")
+	@ShellMethodAvailability("loginCheck")
+	public String whoami() {
+		return String.format("Logged on as: %s", user.getUserName());
+	}
+
+	@ShellMethod("Logout")
+	@ShellMethodAvailability("loginCheck")
+	public String logout() {
+		String name = user.getUserName();
+		this.user = null;
+		return String.format("%s is logged out", name);
+	}
+
+	public Availability loginCheck() {
+		return user == null ? Availability.unavailable("You are not logged on! login first") : Availability.available();
+	}
+
 }
